5c5e61aa2dce956b59516309e7393cbcabfb0c12
teng.gao <247580289@qq.com>
2022-10-16 16:14:44 +0800

    implement the spinner dialog

--- liststudy/src/main/java/com/example/liststudy/listActivity.kt
+++ liststudy/src/main/java/com/example/liststudy/listActivity.kt
@@ -2,6 +2,7 @@ package com.example.liststudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -13,6 +14,7 @@ class listActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
         findViewById<Spinner>(R.id.spinnerId).apply {
             adapter = ArrayAdapter(this@listActivity, R.layout.spinner_item, listData)
             setSelection(2)
@@ -24,13 +26,35 @@ class listActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
-                TODO("Not yet implemented")
+                Log.i("onItemSelected", "")
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
+                Log.i("onNothingSelected", "")
             }
 
         }
+
+        findViewById<Spinner>(R.id.spinnerIdDialog).apply {
+            adapter = ArrayAdapter(this@listActivity, R.layout.spinner_item, listData)
+            setSelection(2)
+        }.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Log.i("onItemSelected", "")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Log.i("onNothingSelected", "")
+            }
+
+        }
+
+
     }
 }
