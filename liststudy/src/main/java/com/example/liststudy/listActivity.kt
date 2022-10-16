c00378d00e7b8bf267b1adfc2aa7a42cd5155d3e
teng.gao <247580289@qq.com>
2022-10-16 16:57:25 +0800

    adapter

--- liststudy/src/main/java/com/example/liststudy/listActivity.kt
+++ liststudy/src/main/java/com/example/liststudy/listActivity.kt
@@ -1,14 +1,21 @@
 package com.example.liststudy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.SimpleAdapter
 import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 
 val listData = listOf("金星", "水星", "木星", "祸心")
+val listDataForSimpleAdapter = listOf(
+    mapOf("name" to "地球", "image" to R.drawable.ic_launcher_foreground),
+    mapOf("name" to "水星", "image" to R.drawable.ic_launcher_background),
+    mapOf("name" to "木星", "image" to R.drawable.ic_launcher_background),
+    mapOf("name" to "祸心", "image" to R.drawable.ic_launcher_foreground)
+)
 
 class listActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +60,13 @@ class listActivity : AppCompatActivity() {
                 Log.i("onNothingSelected", "")
             }
 
+        }
+
+        findViewById<Spinner>(R.id.spinnerIdSimpleAdapterId).apply {
+            adapter = SimpleAdapter(
+                this@listActivity, listDataForSimpleAdapter, R.layout.spinner_simple_adapter,
+                arrayOf("name", "image"), intArrayOf(R.id.textTailer, R.id.imageHeader)
+            )
         }
 
 
